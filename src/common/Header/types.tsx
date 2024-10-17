671a235c3e2511de80c6ee21a5a850b1f5353485
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-10-17 20:01:05 +0200

    Created MovieListResult and MovieResponse interfaces

---
+++ src/common/Header/types.tsx
@@ -0,0 +1,17 @@
+export interface MoviesListResult {
+    id?: number;
+    title?: string;
+    poster_path?: string | null;
+    release_date?: string;
+    backdrop_path?: string | null;
+    genre_ids?: number[];
+    vote_count?: number;
+    vote_average?: number;
+}
+
+export interface MoviesResponse {
+    page?: number;
+    results?: MoviesListResult[];
+    total_results?: number;
+    total_pages?: number;
+}
