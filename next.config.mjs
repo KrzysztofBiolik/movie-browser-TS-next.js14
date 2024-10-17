4a77108dcd6729b4bd7eea1e2da4c6cc03ec0eda
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-10-17 11:49:33 +0200

    Added redirects configuration with destination "/movies" to nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/movies",
+                permanent: false,
+            },
+        ];
+    },
+    reactStrictMode: true,
+    compiler: {
+        styledComponents: true,
+    }
+};
 
 export default nextConfig;
