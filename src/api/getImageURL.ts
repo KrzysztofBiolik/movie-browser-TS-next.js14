350a5b7cf99b7e03c0b49e2b0e708f9530f066aa
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-10-18 14:44:14 +0200

    Added getImageURL function

---
+++ src/api/getImageURL.ts
@@ -0,0 +1,16 @@
+const BASE_URL = "https://image.tmdb.org/t/p/";
+
+interface ImageProps {
+    path: string;
+    size: string;
+}
+const sizes = {
+    medium: "w342",
+    big: "w1280",
+};
+
+export const getImageUrl = ({
+    path,
+    size,
+}: ImageProps
+) => path && `${BASE_URL}${sizes[size as keyof typeof sizes]}${path}}`
