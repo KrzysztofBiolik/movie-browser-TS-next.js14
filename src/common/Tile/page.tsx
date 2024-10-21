bc52713352a5d90200c1d95a87f1795a05061ebf
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-10-21 08:51:49 +0200

    Added title, image and small attribute to Tile component

--- src/common/Tile/page.tsx
+++ src/common/Tile/page.tsx
@@ -1,9 +1,21 @@
-import { Title, Wrapper } from "./styled";
+import { ContentWrapper, Image, Title, Wrapper } from "./styled";
+
+interface TileParametersProps {
+    title?: string,
+    image?: string | null,
+    small?: boolean,
+}
 
 export const Tile = ({
-    title
-}: { title: string | undefined }) => (
-    <Wrapper>
-        <Title>{title}</Title>
+    title,
+    image,
+    small
+}: TileParametersProps
+) => (
+    <Wrapper small={small || undefined}>
+        <Image url={image} />
+        <ContentWrapper>
+            <Title>{title}</Title>
+        </ContentWrapper>
     </Wrapper>
 ) 
--- src/common/Tile/styled.ts
+++ src/common/Tile/styled.ts
@@ -1,6 +1,6 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Wrapper = styled.article`
+export const Wrapper = styled.article<{ small?: boolean }>`
     margin-top: 56px;
     padding: 40px;
     box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
@@ -15,7 +15,30 @@ export const Wrapper = styled.article`
     align-items: start;
     grid-gap: 0 40px;
     border-radius: 5px;
+
+    ${({ small }) => small && css`
+    grid-template-areas:
+    "picture"
+    "meta";
+    grid-template-columns: 1fr;
+    padding: 16px;
+    grid-gap: 16px;
+    `}
 `;
+
+export const ContentWrapper = styled.div`
+    display:grid;
+    grid-template-columns: 1fr;
+    align-content:start;
+`;
+
+export const Image = styled.div<{ url?: string | null }>`
+    padding-top: calc(100%*632/421);
+    background: url(${({ url }) => url});
+    background-size: cover;
+    background-position: center;
+    border-radius: 5px;
+    `;
 
 export const Title = styled.header`
     font-weight: 600;
