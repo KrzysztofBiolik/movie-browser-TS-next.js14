3247b3ff4013d4ee7270239aae2fa4c948c6a5c7
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-10-23 09:03:21 +0200

    Added PeopleListResult and PeopleResponse interfaces

--- src/common/types.tsx
+++ src/common/types.tsx
@@ -15,5 +15,20 @@ export interface MoviesResponse {
     page?: number;
     results?: MoviesListResult[];
     total_results?: number;
-    total_pages?: number | undefined;
+    total_pages?: number;
+}
+
+export interface PeopleListResult {
+    id: number;
+    name?: string;
+    profile_path?: string | null;
+    job?: string | null,
+    character?: string | null,
+}
+
+export interface PeopleResponse {
+    page?: number;
+    results?: PeopleListResult[];
+    total_results?: number;
+    total_pages?: number;
 }
