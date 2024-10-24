8a27df1f1a51de0e0481ad630e1a2f79eeb996e7
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-10-24 11:34:15 +0200

    Added PersonDetails, PersonCast, PersonCrew, PersonCreditsResponse interfaces

--- src/common/types.tsx
+++ src/common/types.tsx
@@ -32,3 +32,47 @@ export interface PeopleResponse {
     total_results?: number;
     total_pages?: number;
 }
+
+export interface PersonDetailsResponse {
+    birthday?: string | null;
+    id?: number;
+    name?: string;
+    biography?: string;
+    place_of_birth?: string | null;
+    profile_path?: string | null;
+    known_for_department?: string;
+}
+
+export interface PersonCast {
+    character?: string;
+    credit_id?: string;
+    release_date?: string;
+    vote_count?: number;
+    vote_average?: number;
+    title?: string;
+    genre_ids?: number[];
+    id: number;
+    job: string | null;
+    poster_path?: string | null;
+}
+
+export interface PersonCrew {
+    id: number;
+    job?: string;
+    vote_count?: number;
+    poster_path?: string | null;
+    title?: string;
+    genre_ids?: number[];
+    vote_average?: number;
+    release_date?: string;
+    credit_id?: string;
+    character?: string;
+}
+
+export interface PersonCreditsResponse {
+    cast?: PersonCast[];
+    crew?: PersonCrew[];
+}
+
+
+
