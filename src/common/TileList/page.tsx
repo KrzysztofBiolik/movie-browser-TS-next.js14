db82789685128d0aef8ee8150f17ba74c2ab238b
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-10-25 13:12:52 +0200

    Added mediaquerries to TileList

--- src/common/TileList/page.tsx
+++ src/common/TileList/page.tsx
@@ -1,19 +1,27 @@
 import Link from "next/link";
 import styled, { css } from "styled-components";
 
-export const TileList = styled.div<{ lessitems?: boolean }, {showJob?: boolean}>`
+export const TileList = styled.div<{ lessitems?: boolean }, { showJob?: boolean }>`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(205px, 1fr));
     grid-gap: 16px;
-    margin-top:32px;
+    margin-top:0;
 
     @media(max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
         grid-template-columns: repeat(2, 1fr);
         margin-top:12px;
     }
 
-    ${({ lessitems }) => lessitems && css`
-        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        ${({ lessitems }) => lessitems && css`
+            grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+
+            @media(max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
+                grid-template-columns: repeat(2, 1fr);
+
+            @media(max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
+                grid-template-columns: 1fr;
+            }
+        }
     `}
 `;
 
