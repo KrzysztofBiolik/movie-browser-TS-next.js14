e4fe9127cc29968890893a860e4ffc2371949dfc
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-10-28 16:53:58 +0100

    Refactored Backdrop

--- src/app/movies/[movieId]/Backdrop/page.tsx
+++ src/app/movies/[movieId]/Backdrop/page.tsx
@@ -4,26 +4,24 @@ import { getImageUrl } from "@/api/getImageURL"
 import { Container } from "@/common/Container/page"
 import { Vote } from "@/common/Vote/page"
 
-export const Backdrop = ({ backdrop }: { backdrop?: MovieDetailsResponse }) => {
-    return (
-        <Wrapper>
-            <Container>
-                <Image url={getImageUrl({
-                    path: backdrop?.backdrop_path,
-                    size: "big",
-                })}>
-                    <Vignette>
-                        <Title>{backdrop?.title}</Title>
-                        <Vote
-                            vote={{
-                                average: backdrop?.vote_average,
-                                count: backdrop?.vote_count,
-                            }}
-                            backdrop
-                        />
-                    </Vignette>
-                </Image>
-            </Container>
-        </Wrapper>
-    )
-}
+export const Backdrop = ({ backdrop }: { backdrop?: MovieDetailsResponse }) => (
+    <Wrapper>
+        <Container>
+            <Image url={getImageUrl({
+                path: backdrop?.backdrop_path,
+                size: "big",
+            })}>
+                <Vignette>
+                    <Title>{backdrop?.title}</Title>
+                    <Vote
+                        vote={{
+                            average: backdrop?.vote_average,
+                            count: backdrop?.vote_count,
+                        }}
+                        backdrop
+                    />
+                </Vignette>
+            </Image>
+        </Container>
+    </Wrapper>
+)
